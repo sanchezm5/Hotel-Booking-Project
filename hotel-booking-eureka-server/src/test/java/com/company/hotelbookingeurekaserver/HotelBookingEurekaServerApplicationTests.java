769af4f5aacc456636850c90efd2d5794da85e43
Maria Sanchez <msanchez157@gmail.com>
2019-10-22 15:45:08 -0400

    Added the eureka server

---
+++ hotel-booking-eureka-server/src/test/java/com/company/hotelbookingeurekaserver/HotelBookingEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.hotelbookingeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookingEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
