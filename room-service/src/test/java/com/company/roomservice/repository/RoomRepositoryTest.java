0cb9d30d0308e5bc25ee2aacc29a2e345b485049
Maria Sanchez <msanchez157@gmail.com>
2019-10-22 16:54:47 -0400

    Created room service project

---
+++ room-service/src/test/java/com/company/roomservice/repository/RoomRepositoryTest.java
@@ -0,0 +1,18 @@
+//package com.company.roomservice.repository;
+//
+//import org.junit.Before;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//class RoomRepositoryTest {
+//
+//    @Autowired RoomRepository roomRepo;
+//
+//    @Before
+//
+//}
