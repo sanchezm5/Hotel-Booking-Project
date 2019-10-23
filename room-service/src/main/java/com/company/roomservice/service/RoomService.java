8095f5124afb80e75993932c4b4c032655e67863
Maria Sanchez <msanchez157@gmail.com>
2019-10-23 11:34:07 -0400

    Added viewmodel and service layer files

---
+++ room-service/src/main/java/com/company/roomservice/service/RoomService.java
@@ -0,0 +1,6 @@
+package com.company.roomservice.service;
+
+public class RoomService {
+
+
+}
---
+++ room-service/src/main/java/com/company/roomservice/viewmodel/RoomViewModel.java
@@ -0,0 +1,6 @@
+package com.company.roomservice.viewmodel;
+
+import com.company.roomservice.model.Room;
+
+public class RoomViewModel extends Room {
+}
