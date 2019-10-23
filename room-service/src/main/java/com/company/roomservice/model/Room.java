e58fc9012c1347d42b235ca62934253f45d8f325
Asiyat Kush <Alicia@AMINATs-MacBook-Pro.local>
2019-10-23 11:06:13 -0400

    Unit/Integrationtests completed

--- room-service/src/main/java/com/company/roomservice/model/Room.java
+++ room-service/src/main/java/com/company/roomservice/model/Room.java
@@ -7,6 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
@@ -41,5 +42,20 @@ public class Room implements Serializable {
 
     public void setOccupant(String occupant) {
         this.occupant = occupant;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Room room = (Room) o;
+        return Objects.equals(getId(), room.getId()) &&
+                Objects.equals(getNumber(), room.getNumber()) &&
+                Objects.equals(getOccupant(), room.getOccupant());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getNumber(), getOccupant());
     }
 }
--- room-service/src/test/java/com/company/roomservice/repository/RoomRepositoryTest.java
+++ room-service/src/test/java/com/company/roomservice/repository/RoomRepositoryTest.java
@@ -1,18 +1,119 @@
-//package com.company.roomservice.repository;
-//
-//import org.junit.Before;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//class RoomRepositoryTest {
-//
-//    @Autowired RoomRepository roomRepo;
-//
-//    @Before
-//
-//}
+package com.company.roomservice.repository;
+
+        import com.company.roomservice.model.Room;
+        import org.junit.Before;
+        import org.junit.Test;
+        import org.junit.runner.RunWith;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.boot.test.context.SpringBootTest;
+        import org.springframework.test.context.junit4.SpringRunner;
+        import org.springframework.transaction.annotation.Transactional;
+        import java.util.List;
+        import java.util.Optional;
+
+        import static org.junit.Assert.*;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RoomRepositoryTest {
+
+    @Autowired RoomRepository roomRepo;
+
+    @Before
+    public void setUp(){
+        roomRepo.deleteAll();
+    }
+
+    @Test
+    @Transactional
+    public void addGetDeleteRoom(){
+
+        //Arrange
+        Room room = new Room();
+        room.setNumber(100);
+        room.setOccupant("Jane Smith");
+
+        //Act
+        room = roomRepo.save(room);
+        Optional<Room> fromRepo = roomRepo.findById(room.getId());
+
+        //Assert
+        assertEquals(room, fromRepo.get());
+
+        roomRepo.deleteById(room.getId());
+        fromRepo = roomRepo.findById(room.getId());
+        assertFalse(fromRepo.isPresent());
+
+    }
+
+    @Test
+    public void findAllRooms(){
+
+        Room room1 = new Room();
+        room1.setNumber(100);
+        room1.setOccupant("Jane Smith");
+
+        roomRepo.save(room1);
+
+        Room room2 = new Room();
+        room2.setNumber(101);
+        room2.setOccupant("Vacant");
+
+        roomRepo.save(room2);
+
+       List<Room> rooms = roomRepo.findAll();
+       assertEquals(rooms.size(), 2);
+       assertEquals(rooms.get(0), room1);
+
+    }
+
+    @Test
+    @Transactional
+    public void updateRoom(){
+        //Arrange
+        Room room = new Room();
+        room.setNumber(100);
+        room.setOccupant("Jane Smith");
+
+        roomRepo.save(room);
+
+        room.setOccupant("Vacant");
+        roomRepo.save(room);
+
+        Room room1 = roomRepo.getOne(room.getId());
+        assertEquals(room, room1);
+
+        //** this achieves the same result;
+        //Room room2 = roomRepo.findById(room.getId()).get();
+        //assertEquals(room, room2);
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
